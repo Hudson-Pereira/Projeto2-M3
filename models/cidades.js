b0f4f642a939d892e585e781b14e61ce8d8e3c13
Hudson-Pereira <hudson.o.pereira@gmail.com>
2021-11-08 15:16:04 -0300

    inserido data de criacao na cidade e pais

--- models/cidades.js
+++ models/cidades.js
@@ -4,7 +4,8 @@ const cidadeModel = new mongoose.Schema({
   nome: { type: String, required: true },
   qtdDeBairros: { type: Number, required: true },
   populacao: { type: Number, required: true },
-  aniversarioCidade: { type: Date, required:true }
+  aniversarioCidade: { type: Date, required: true },
+  dataCriacao: { type: Date, default: Date.now },
 });
 
 const Cidade = mongoose.model("Cidades", cidadeModel);
--- models/estados.js
+++ models/estados.js
@@ -4,7 +4,7 @@ const estadoModel = new mongoose.Schema({
   nome: { type: String, required: true },
   regiao: { type: String, required: true },
   populacao: { type: Number, required: true },
-  valorSalMin: { type: Number },
+  valorSalMin: { type: Number, required: true },
   dataCriacao: { type: Date, default: Date.now },
 });
 
