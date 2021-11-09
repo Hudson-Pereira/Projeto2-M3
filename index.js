f49530022cb7348b43e0a007722a83f19edc2a3b
Felipe Wrany <felipe.wrany@gmail.com>
2021-11-08 22:39:59 -0300

    ajustes e correções

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const Connected = require("./models/conn/index");
 app.use(express.json());
 
 Connected();
-const port = 3000;
+//https://projeto2-m3.herokuapp.com/
+
+app.get("/", (req, res) => { //Rota geral
+  res.status(200).json({ message: "Rota geral do projeto funcionando" });
+});
 
 const routerEstados = require("./router/estados.routes");
 app.use("/estados", routerEstados);
